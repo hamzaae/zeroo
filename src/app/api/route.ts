243859f9cefab9af9992e4bcf606c782b6f9eeab
hamzaae <motassimhamza99@gmail.com>
2024-09-21 00:45:34 +0100

    upload file issues

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -32,7 +32,7 @@ export async function POST(req: NextRequest) {
     const model = new ChatCohere({
       apiKey: process.env.COHERE_API_KEY
     });
-    console.log("Model created");
+    // console.log("Model created");
 
     const prompt = "Given the following text, which is a summary of a document, generate a quizz based on the text. Return json only that contains a quizz object with the fields: name, description and questions. The questions is an array of objects with fields: questionText, answers. The answers field should be an array of objects with the fields: answerText, isCorrect.";
 
