18a6cf9185d7550a834699dc024b8f4a3f7e9f93
hamzaae <motassimhamza99@gmail.com>
2024-09-21 18:47:58 +0100

    move api to client side

--- src/app/start/UploadFile.tsx
+++ src/app/start/UploadFile.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { NextResponse } from "next/server";
+import { ChatCohere } from "@langchain/cohere";
+import { HumanMessage } from "@langchain/core/messages";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -43,22 +48,46 @@ export default function UploadFile({ setQuizz }: UploadFileProps) {
       return;
     }
     setIsLoading(true);
-    const formData = new FormData();
-    formData.append("pdf", file as Blob);
+
     try {
-      const response = await fetch("/api", {
-        method: "POST",
-        body: formData,
-      });
-      if (response.ok) {
-        const data = await response.json();
-        setQuizz(data.quizzObject.quiz); 
-      } else {
-        toast({
-          title: "An error occurred",
-          description: "Please try again later",
-        });
-      }
+    console.log("Uploading file");
+    const pdfLoader = new PDFLoader(file as Blob, {
+        parsedItemSeparator: " "
+    });
+    const docs = await pdfLoader.load();
+    // console.log(docs);
+
+    const selectedDocuments = docs.filter((doc) => doc.pageContent !== undefined);
+    const texts = selectedDocuments.map((doc) => doc.pageContent);
+    // console.log(texts);
+
+    if (!process.env.COHERE_API_KEY) {
+      console.log("API key not found");
+      return;
+      // return NextResponse.json({ message: "API key not found" }, { status: 500 });
+    }
+    
+    const model = new ChatCohere({
+      apiKey: process.env.COHERE_API_KEY
+    });
+    // console.log("Model created");
+
+    const prompt = "Given the following text, which is a summary of a document, generate a quizz based on the text. Return json only that contains a quizz object with the fields: name, description and questions. The questions is an array of objects with fields: questionText, answers. The answers field should be an array of objects with the fields: answerText, isCorrect.";
+
+    const message = new HumanMessage(prompt + "\n" + texts.join("\n"));
+    // console.log(message);
+
+    const results = await model.invoke([message]);
+    // console.log(results.content);
+
+    const responseText = (results.content as string).replace(/^```json\s*/, '').replace(/\s*```$/, '');
+    const quizzObject = JSON.parse(responseText);
+    // console.log(quizzObject.quiz); 
+
+    // return NextResponse.json({ quizzObject }, { status: 200 });
+
+    setQuizz(quizzObject.quiz); 
+
     } catch (e) {
       console.log(e);
       toast({
